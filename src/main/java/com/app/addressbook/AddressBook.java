58b7132bee8f415edcc9d425980afbb82880aaaf
Arif-Iquba'l <arif04102001@gmail.com>
2026-03-09 23:31:00 +0530

    [Arif Iqubal] : UC2: Added functionality to create a new Contact using console input from AddressBookMain and managed relationship between AddressBook and ContactPerson using OOP concepts.

--- src/main/java/com/app/addressbook/AddressBook.java
+++ src/main/java/com/app/addressbook/AddressBook.java
@@ -1,5 +1,20 @@
 package com.app.addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
 
+	private List<ContactPerson> contactList = new ArrayList<>();
+
+	public void addContact(ContactPerson person) {
+		contactList.add(person);
+		System.out.println("Contact added successfully.");
+	}
+
+	public void displayContacts() {
+		for (ContactPerson person : contactList) {
+			person.displayContact();
+		}
+	}
 }
--- src/main/java/com/app/addressbook/AddressBookMain.java
+++ src/main/java/com/app/addressbook/AddressBookMain.java
@@ -1,10 +1,44 @@
 package com.app.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Address Book Program");
 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter First Name: ");
+		String firstName = scanner.nextLine();
+
+		System.out.print("Enter Last Name: ");
+		String lastName = scanner.nextLine();
+
+		System.out.print("Enter Address: ");
+		String address = scanner.nextLine();
+
+		System.out.print("Enter City: ");
+		String city = scanner.nextLine();
+
+		System.out.print("Enter State: ");
+		String state = scanner.nextLine();
+
+		System.out.print("Enter Zip: ");
+		String zip = scanner.nextLine();
+
+		System.out.print("Enter Phone Number: ");
+		String phoneNumber = scanner.nextLine();
+
+		System.out.print("Enter Email: ");
+		String email = scanner.nextLine();
+
+		ContactPerson person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact(person);
+
+		addressBook.displayContacts();
 	}
 }
